6ce65b1f238f3ae7a8e74d0ea1099d4a31477ecb
Octosquidport <deluns@gmail.com>
2015-03-02 23:44:04 -0500

    Just some room stuffs

--- TextRPG/src/mapInternals/GameMap.java
+++ TextRPG/src/mapInternals/GameMap.java
@@ -3,5 +3,18 @@ package mapInternals;
  * Contains all the rooms in the game using an array or something like that
  */
 public class GameMap {
+	private Room[] rooms;
+	public static final int howManyRooms = 25; // it could be any other number we decide
 	
+	GameMap()
+	{
+		int iterator;
+		
+		rooms = new Room[howManyRooms];
+		
+		for (iterator = 0; iterator < howManyRooms; ++iterator)
+		{
+			rooms[iterator] = new Room();
+		}
+	}
 }
--- TextRPG/src/mapInternals/Room.java
+++ TextRPG/src/mapInternals/Room.java
@@ -1,5 +1,49 @@
 package mapInternals;
 
 public class Room {
-
+	private int brightness;
+	private boolean[] exitDirections; // 0 - North, 1 - West, 2 - South, 3 - East
+	
+	Room()
+	{
+		int iterator;
+		
+		brightness = 3;
+		exitDirections = new boolean[4];
+		
+		for (iterator = 0; iterator < 4; ++iterator)
+		{
+			exitDirections[iterator] = false;
+		}
+	}
+	
+	Room(int lightLevel, boolean northExit, boolean westExit, boolean southExit, boolean eastExit)
+	{
+		int iterator;
+		
+		brightness = lightLevel;
+		exitDirections = new boolean[4];
+		
+		exitDirections[0] = northExit;
+		exitDirections[1] = westExit;
+		exitDirections[2] = southExit;
+		exitDirections[3] = eastExit;		
+	}
+	
+	public void setNorthExit(boolean isExit)
+	{
+		exitDirections[0] = isExit;
+	}
+	public void setWestExit(boolean isExit)
+	{
+		exitDirections[1] = isExit;
+	}
+	public void setSouthExit(boolean isExit)
+	{
+		exitDirections[2] = isExit;
+	}
+	public void setEastExit(boolean isExit)
+	{
+		exitDirections[3] = isExit;
+	}
 }
