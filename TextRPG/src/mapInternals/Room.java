dcf2a2284418f03da9b9c8b4d420ca949bbe9c14
octosquidport <deluns@gmail.com>
2015-03-03 10:26:25 -0500

    edited room class

--- TextRPG/src/mapInternals/Room.java
+++ TextRPG/src/mapInternals/Room.java
@@ -4,47 +4,21 @@ import java.util.ArrayList;
 
 public class Room {
 	private int brightness;
-	private boolean[] exitDirections; // 0 - North, 1 - West, 2 - South, 3 - East
-	private ArrayList<Room> exitRooms;
+	private ArrayList<Door> exitDoors = new ArrayList<Door>();
 	
 	Room()
 	{
-		
 		brightness = 3;
-		exitDirections = new boolean[4];
-		
-		for (int i = 0; i < 4; ++i)
-		{
-			exitDirections[i] = false;
-		}
 	}
 	
-	Room(int lightLevel, Room...rooms)
+	Room(int lightLevel, Door... doors)
 	{
 		
 		brightness = lightLevel;
-		exitDirections = new boolean[4];
 		
-		exitDirections[0] = northExit;
-		exitDirections[1] = westExit;
-		exitDirections[2] = southExit;
-		exitDirections[3] = eastExit;		
+		for (Door exit : doors)
+			exitDoors.add(exit);
 	}
 	
-	public void setNorthExit(boolean isExit)
-	{
-		exitDirections[0] = isExit;
-	}
-	public void setWestExit(boolean isExit)
-	{
-		exitDirections[1] = isExit;
-	}
-	public void setSouthExit(boolean isExit)
-	{
-		exitDirections[2] = isExit;
-	}
-	public void setEastExit(boolean isExit)
-	{
-		exitDirections[3] = isExit;
-	}
+	
 }
