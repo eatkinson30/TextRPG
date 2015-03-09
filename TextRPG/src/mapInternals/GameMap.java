d2220937dfe50d56b1038fe58730b05f150217db
Octosquidport <deluns@gmail.com>
2015-03-09 02:17:44 -0400

    added a small amount of stuff to the game map

--- TextRPG/src/mapInternals/GameMap.java
+++ TextRPG/src/mapInternals/GameMap.java
@@ -2,6 +2,8 @@ package mapInternals;
 
 import java.util.ArrayList;
 
+import main.FL.Direction;
+
 /*
  * Contains all the rooms in the game using an array or something like that
  */
@@ -12,12 +14,14 @@ public class GameMap {
 	
 	GameMap()
 	{
+		Door temp;
+		
 		rooms = new ArrayList<> (howManyRooms);
 		doors = new ArrayList<> (25);
 		
 		// Initializes Rooms //
-		rooms.add(new Room("Lobby", 
-				"Description here", 3));
+		rooms.add(new Room("Lobby",
+				"This is the lobby, the first room that you came to when you entered the facility. The main entrance lies to the north", 3));
 		
 		rooms.add(new Room("West Wing",
 				"", 3));
@@ -30,6 +34,17 @@ public class GameMap {
 		
 		// Creates doors to connect the rooms //
 		
+		temp = new Door(rooms.get(0), rooms.get(1), Direction.WEST, Direction.EAST); // Connects Lobby and West Wing
+		rooms.get(0).GetAllDoors().add(temp);
+		rooms.get(1).GetAllDoors().add(temp);
+		
+		temp = new Door(rooms.get(1), rooms.get(2), Direction.SOUTH, Direction.NORTH); // Connects West Wing and Library
+		rooms.get(1).GetAllDoors().add(temp);
+		rooms.get(2).GetAllDoors().add(temp);
+		
+		temp = new Door(rooms.get(2), rooms.get(3), Direction.NORTH, Direction.SOUTH); // Connects Closet and Library
+		rooms.get(2).GetAllDoors().add(temp);
+		rooms.get(3).GetAllDoors().add(temp);
 		
 	}
 }
