1b58ef5c553c4e90e3f7e14af82a96df6620353a
octosquidport <deluns@gmail.com>
2015-04-01 10:14:03 -0400

    Implemented Standarad Actions, Examine and Drop

--- TextRPG/src/items/Item.java
+++ TextRPG/src/items/Item.java
@@ -3,6 +3,7 @@ package items;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import rpgObjectClasses.Actor;
 import rpgObjectClasses.Entity;
 import main.FL;
 import mapInternals.Room;
@@ -11,12 +12,6 @@ public abstract class Item extends Entity
 {
 	protected boolean carried;
 	protected ArrayList<String> itemUses = new ArrayList<String>(Arrays.asList("Examine", "Drop"));
-	
-	protected void ExamineAction()
-	{
-		FL.PrintL(""); // add to this
-	}
-	
 	
 	public Item(Room startingLocation, String name)
 	{
@@ -45,19 +40,29 @@ public abstract class Item extends Entity
 		return itemUses;
 	}
 	
-	public void executeAction(int option)
+	public void executeAction(int option) // Chooses which method to call based on a number representing a choice made by the player to perform a certain action on the item
 	{
 		switch (option)
 		{
-		case 0:
-			
-		
+		default:
 		}
 	}
 	
-	static public void move(Item theThing, ItemCarrier source, ItemCarrier destination) // Moves item from source into the destination
+	// Action handlers //
+	protected void ExamineAction() // Description for item // override this in subclasses
 	{
-		source.removeItem(theThing);
-		destination.addItem(theThing);
+		FL.PrintL("");
+	}
+	
+	protected void DropAction(Actor character) // Removes item from inventory to the enclosing Room
+	{
+			move(character, character.WhereBeThis());
+	}
+	// // //
+	
+	public void move(ItemCarrier source, ItemCarrier destination) // Moves item from source into the destination
+	{
+		source.removeItem(this);
+		destination.addItem(this);
 	}	
 }
--- TextRPG/src/items/Keycard.java
+++ TextRPG/src/items/Keycard.java
@@ -6,7 +6,6 @@ import mapInternals.Room;
 public class Keycard extends Item {
 	private FL.Color keyColor; 
 		
-	
 	public Keycard(Room startingLocation, String name, FL.Color color) {
 		super(startingLocation, name);
 		
